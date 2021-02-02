534a4af0fa8fd853d5144a6448df68caf3b0b981
Antonio López Martínez-Carrasco <antoniolopezmc1995@gmail.com>
2021-02-02 19:31:59 +0100

    Initial structure of the project created.

---
+++ setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="subgroup-discovery",
+    version="0.0.1",
+    author="Antonio López Martínez-Carrasco",
+    author_email="antoniolopezmc1995@gmail.com",
+    description="subgroup-discovery is a python library which contains a collection of subgroup discovery algorithms.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/antoniolopezmc/subgroup-discovery",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: BSD License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
