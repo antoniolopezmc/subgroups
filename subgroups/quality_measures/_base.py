0df547cfd6b906e8df6ece72154df263697f537d
Antonio López Martínez-Carrasco <antoniolopezmc1995@gmail.com>
2021-02-22 21:38:52 +0100

    More comments with other parameters/metrics added.

--- subgroups/quality_measures/_base.py
+++ subgroups/quality_measures/_base.py
@@ -24,6 +24,17 @@ class QualityMeasure(ABC):
     # False Positives FP (rows not covered by the target).
     SUBGROUP_PARAMETER_FP = "FP"
     
+    ### IMPORTANT ###
+    ## There are other elements/parameters which are also used in the Subgroup Discovery technique. We define the following equivalences:
+    #
+    # n = tp + fp
+    # N = TP + FP
+    # p = tp / (tp + fp)
+    # p0 = TP / (TP + FP)
+    # tn = FP - fp
+    # fn = TP - tp
+    #
+    
     @abstractmethod
     def compute(self, dict_of_parameters):
         pass
