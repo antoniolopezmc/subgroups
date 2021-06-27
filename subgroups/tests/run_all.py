fb8b00cfba2b8ff9da3a2f4629b779fbdf443271
Antonio López Martínez-Carrasco <antoniolopezmc1995@gmail.com>
2021-06-28 00:57:49 +0200

    Test files: name changed.

--- subgroups/tests/run_all.py
+++ subgroups/tests/run_all.py
@@ -6,25 +6,25 @@
 """Run all tests.
 """
 
-from subgroups.tests.core.operator_test import *
-from subgroups.tests.core.selector_test import *
-from subgroups.tests.core.pattern_test import *
-from subgroups.tests.core.subgroup_test import *
-from subgroups.tests.quality_measures.all_tests import *
-from subgroups.tests.utils.dataframe_filters_test import *
-from subgroups.tests.data_structures.fp_tree_node_test import *
-from subgroups.tests.data_structures.fp_tree_for_sdmap_test import *
-from subgroups.tests.algorithms.individual_subgroups.non_numeric_target.sdmap_test import *
-from subgroups.tests.data_structures.vertical_list_test import *
-from subgroups.tests.algorithms.individual_subgroups.non_numeric_target.vlsd_test import *
+from subgroups.tests.core.test_operator import *
+from subgroups.tests.core.test_selector import *
+from subgroups.tests.core.test_pattern import *
+from subgroups.tests.core.test_subgroup import *
+from subgroups.tests.quality_measures.test_all import *
+from subgroups.tests.utils.test_dataframe_filters import *
+from subgroups.tests.data_structures.test_fp_tree_node import *
+from subgroups.tests.data_structures.test_fp_tree_for_sdmap import *
+from subgroups.tests.algorithms.individual_subgroups.non_numeric_target.test_sdmap import *
+from subgroups.tests.data_structures.test_vertical_list import *
+from subgroups.tests.algorithms.individual_subgroups.non_numeric_target.test_vlsd import *
 
 if __name__ == "__main__":
-    # core/operator_test file.
+    # core/test_operator file.
     test_Operator_evaluate_method()
     test_Operator_evaluate_method_with_pandasSeries()
     test_Operator_generate_from_str_method()
     test_Operator_string_representation()
-    # core/selector_test file.
+    # core/test_selector file.
     test_Selector_creation_process()
     test_Selector_deletion_process()
     test_Selector_same_value_different_type()
@@ -32,20 +32,20 @@ if __name__ == "__main__":
     test_Selector_match_method()
     test_Selector_generate_from_str_method()
     test_Selector_comparisons()
-    # core/pattern_test file.
+    # core/test_pattern file.
     test_Pattern()
     test_Pattern_is_contained_method()
-    # core/subgroup_test file.
+    # core/test_subgroup file.
     test_Subgroup()
     test_Subgroup_filter()
-    # quality_measures/all_tests file.
+    # quality_measures/test_all file.
     test_quality_measures()
     test_quality_measures_compute()
-    # utils/dataframe_filters_test file.
+    # utils/test_dataframe_filters file.
     test_dataframe_filters()
-    # data_structures/fp_tree_node_test file.
+    # data_structures/test_fp_tree_node file.
     test_FPTreeNode()
-    # data_structures/fp_tree_for_sdmap_test file.
+    # data_structures/test_fp_tree_for_sdmap file.
     test_FPTreeForSDMap_generate_set_of_frequent_selectors_1()
     test_FPTreeForSDMap_generate_set_of_frequent_selectors_2()
     test_FPTreeForSDMap_generate_set_of_frequent_selectors_3()
@@ -55,7 +55,7 @@ if __name__ == "__main__":
     test_FPTreeForSDMap_build_tree_4()
     test_FPTreeForSDMap_generate_conditional_fp_tree_1()
     test_FPTreeForSDMap_generate_conditional_fp_tree_2()
-    # algorithms/sdmap_test file.
+    # algorithms/test_sdmap file.
     test_SDMap_init_method_1()
     test_SDMap_init_method_2()
     test_SDMap_fpgrowth_method_1()
@@ -74,12 +74,12 @@ if __name__ == "__main__":
     test_SDMap_fit_method_10()
     test_SDMap_fit_method_11()
     test_SDMap_visited_and_pruned_nodes()
-    # data_structures/vertical_list_test file.
+    # data_structures/test_vertical_list file.
     test_vertical_list_1()
     test_vertical_list_2()
     test_vertical_list_3()
     test_vertical_list_str_method()
-    # algorithms/vlsd file.
+    # algorithms/test_vlsd file.
     test_VLSD_init_method_1()
     test_VLSD_init_method_2()
     test_VLSD_fit_method_1()
