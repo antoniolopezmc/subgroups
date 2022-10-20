8e517f503d6b3bbd416fb8ef5b3d3829f351dd7a
Antonio López Martínez-Carrasco <anlopezmc@gmail.com>
2022-10-20 18:06:39 +0100

    Datasets added.

--- subgroups/__init__.py
+++ subgroups/__init__.py
@@ -2,6 +2,7 @@ import subgroups.algorithms
 import subgroups.core
 import subgroups.data_structures
 import subgroups.quality_measures
+import subgroups.datasets
 
 # Library information.
-__version__ = "0.0.4"
+__version__ = "0.0.5"
